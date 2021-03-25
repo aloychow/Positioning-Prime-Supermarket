cf821dd9509c6328785539a842d595ec2fdab232
aloychow <aloychow@outlook.com>
2021-03-25 16:02:16 +0800

    updated table

--- index.py
+++ index.py
@@ -6,13 +6,6 @@ from dash.dependencies import Input, Output
 # Connect to main app.py file
 from app import app, server
 
-# Connect to app pages (in apps)
-from apps import homepage
-from apps import portfolio_optimisation
-from apps.technical_analysis_patterns import technical_analysis_candlestick, technical_analysis_indicators
-from apps.sentiment_analysis import sentiment_analysis_twitter, analysis_reddit
-from apps.predictions.time_series import time_series_forecasting
-
 from assets.styles import CONTENT_STYLE
 from sidebar import sidebar
 
@@ -35,21 +28,7 @@ app.layout = dbc.Container([
               [Input('url', 'pathname')])
 def display_page(pathname):
     if pathname == '/apps/homepage':
-        return homepage.layout
-    if pathname == '/apps/portfolio_optimisation':
-        return portfolio_optimisation.layout
-    if pathname == '/apps/technical_analysis_patterns/technical_analysis_candlesticks':
-        return technical_analysis_candlestick.layout
-    if pathname == '/apps/technical_analysis_patterns/technical_analysis_indicators':
-        return technical_analysis_indicators.layout
-    if pathname == '/apps/sentiment_analysis/analysis_reddit':
-        return analysis_reddit.layout
-    if pathname == '/apps/sentiment_analysis/sentiment_analysis_twitter':
-        return sentiment_analysis_twitter.layout
-    if pathname == '/apps/predictions/time_series/time_series_forecasting':
-        return time_series_forecasting.layout
-    else:
-        return technical_analysis_indicators.layout  # Main page
+        pass
 
 
 if __name__ == '__main__':
--- sidebar.py
+++ sidebar.py
@@ -5,7 +5,7 @@ from assets.styles import SIDEBAR_STYLE
 
 sidebar = html.Div(
     [
-        html.H5("STOCKSTOCKSTOCKS", className="text-center text-white"),
+        html.H5("Supermarket Analysis", className="text-center text-white"),
         html.Hr(style={
             'background-color': 'white'
         }),
@@ -16,34 +16,26 @@ sidebar = html.Div(
             [
                 dbc.NavLink("Home", href="/apps/homepage"),
                 
-                dbc.NavLink("Portfolio Optimisation", href="/apps/portfolio_optimisation"),
+                dbc.NavLink("1", href="/apps/1"),
                 
                 dbc.DropdownMenu([
-                    dbc.NavLink("Candlestick Patterns",
-                                href="/apps/technical_analysis_patterns/technical_analysis_candlesticks",
+                    dbc.NavLink("2.1",
+                                href="/apps/2.1",
                                 active="exact"),
-                    dbc.NavLink("Statistical Indicators",
-                                href="/apps/technical_analysis_patterns/technical_analysis_indicators",
+                    dbc.NavLink("2.2",
+                                href="/apps/2.2",
                                 active="exact")],
-                    label="Technical Indicators",
+                    label="2",
                     nav=True,
                 ),
 
                 dbc.DropdownMenu([
-                    dbc.NavLink("Reddit", href="/apps/sentiment_analysis/analysis_reddit"),
-                    dbc.NavLink("Twitter", href="/apps/sentiment_analysis/sentiment_analysis_twitter"),
+                    dbc.NavLink("3.1", href="/apps/sentiment_analysis/analysis_reddit"),
+                    dbc.NavLink("3.2", href="/apps/sentiment_analysis/sentiment_analysis_twitter"),
                 ],
                     label="Sentiment Analysis",
                     nav=True,
                 ),
-
-                dbc.DropdownMenu([
-                    dbc.NavLink("Time Series Forecasting", href="/apps/predictions/time_series/time_series_forecasting"),
-                    # dbc.NavLink("ARIMA", href="/apps/sentiment_analysis/sentiment_analysis_twitter"),
-                ],
-                    label="Machine Learning",
-                    nav=True,
-                )
             ],
             vertical=True,
             # pills=True,
